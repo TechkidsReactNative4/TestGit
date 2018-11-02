6c6b04250a013347a46dee00fcf411eaf43bbedc
Đức Thiên <ducthien@ucs-MacBook-Pro.local>
2018-11-02 20:20:43 +0700

    add function swap

--- src/qika.js
+++ src/qika.js
@@ -1,2 +1,13 @@
-abcd
-hello from the other side 
+var a = 4;
+var b = 3;
+
+console.log("a: " + a)
+console.log("b: " + b)
+
+a = a + b;
+b = a - b;
+a = a - b;
+
+console.log("\nAfter swap:\n")
+console.log("a: " + a)
+console.log("b: " + b)
